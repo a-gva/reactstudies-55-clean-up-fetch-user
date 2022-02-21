25f1e1a46874da1a87dedcd1f203c9c3dda9ebbe
Alexy Araujo <96698163+a-gva@users.noreply.github.com>
2022-02-21 10:12:02 -0300

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+    return (
+        <div>
+            {/* Search Component */}
+            <UserResults />
+        </div>
+    )
+}
+
+export default Home;
